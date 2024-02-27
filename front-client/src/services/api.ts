d1940bac849ac7a79c1107e6b4806a467c4ed6ed
aureasiqueira1 <aurea@gec.inatel.br>
2024-02-27 16:00:01 -0300

    Alterando url do front para deploy

--- front-client/src/services/api.ts
+++ front-client/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/cliente",
+  // baseURL: "http://localhost:3001/cliente",
+  baseURL: "https://api-client.vercel.app/cliente",
 });
